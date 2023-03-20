8d320cd4ac06ca959edd76a6a281e9749c36e040
Richard Davidsson <r-1chard@hotmail.com>
2023-03-20 11:55:47 +0100

    added cancel button to checkout

--- main.js
+++ main.js
@@ -227,3 +227,8 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+//testing close clear function
+function clearForm(){
+    document.getElementById("form").reset();
+}
